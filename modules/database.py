ee6d2c35b1a24f9451a9bf7e067071feebad3bbf
kylelorenzana01-pixel <kylelorenzana01@gmail.com>
2026-06-06 16:33:48 +0800

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -2,12 +2,13 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # kukunin ang mga variable mula sa .env file
+load_dotenv()  # loads variables from .env file (for local development)
 
 def db_connection():
     try:
         conn = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
+            port=os.getenv('DB_PORT', 3306),           
             user=os.getenv('DB_USER', 'root'),
             password=os.getenv('DB_PASSWORD', ''),
             database=os.getenv('DB_NAME', 'bp_system_db')
